42ba413416936ec2822dee916225fa156c3c2e83
gh-actions <actions@github.com>
2022-04-19 14:18:30 +0000

    Update for 0.1.4

--- scala-cli.rb
+++ scala-cli.rb
@@ -5,9 +5,9 @@
 class ScalaCli < Formula
   desc "Launcher for ScalaCli"
   homepage "https://virtuslab.github.io/scala-cli/"
-  url "https://github.com/Virtuslab/scala-cli/releases/download/v0.1.3/scala-cli-x86_64-apple-darwin.gz"
-  version "0.1.3"
-  sha256 "5250c5d8469a7adeb6cd9574cfa5ce66f83f61e07ffc05cf0e78bc87759ceabc"
+  url "https://github.com/Virtuslab/scala-cli/releases/download/v0.1.4/scala-cli-x86_64-apple-darwin.gz"
+  version "0.1.4"
+  sha256 "be171861f8fae6aa21a9379fb0cb01f61ccd2ebaf04b4b931b3da334ee747aa5"
   license "Apache-2.0"
 
   def install
