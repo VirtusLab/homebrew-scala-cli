0197a76868bbcb266e61b2286197a18a8080b00e
Marek Moryl <moryl.moryl@gmail.com>
2022-10-26 14:46:26 +0200

    Support for M1 architecture

--- scala-cli.rb
+++ scala-cli.rb
@@ -5,13 +5,21 @@
 class ScalaCli < Formula
   desc "Launcher for ScalaCli"
   homepage "https://virtuslab.github.io/scala-cli/"
-  url "https://github.com/Virtuslab/scala-cli/releases/download/v0.1.16/scala-cli-x86_64-apple-darwin.gz"
+  url (RUBY_PLATFORM.include? "arm64") ?
+    "https://github.com/Virtuslab/scala-cli/releases/download/v0.1.16/scala-cli-aarch64-apple-darwin.gz" :
+    "https://github.com/Virtuslab/scala-cli/releases/download/v0.1.16/scala-cli-x86_64-apple-darwin.gz"
   version "0.1.16"
-  sha256 "22fddca9d13717ec39d33db51db07aa621b4179f09db744a9ebe2240194bdded"
+  sha256 (RUBY_PLATFORM.include? "arm64") ?
+    "386e2031cca3671783079e5e8151284b8f33eec8b50936ed9d58372382d3d2e7" :
+    "22fddca9d13717ec39d33db51db07aa621b4179f09db744a9ebe2240194bdded"
   license "Apache-2.0"
 
   def install
-    bin.install "scala-cli-x86_64-apple-darwin" => "scala-cli"
+    if (RUBY_PLATFORM.include? "arm64")
+      bin.install "scala-cli-aarch64-apple-darwin" => "scala-cli"
+    else
+      bin.install "scala-cli-x86_64-apple-darwin" => "scala-cli"
+    end
   end
 
   test do
